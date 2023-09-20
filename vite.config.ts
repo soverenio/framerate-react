d739c6e42f94ec6750ccce8a324fa4b9cdb7bcd1
Vitaly Afonin <afoninv@mail.ru>
2023-09-20 14:55:58 +0300

    Generate Typescript definitions for library (with 'vite-plugin-dts')

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +20,7 @@ export default defineConfig({
     },
   },
 
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
 
   test: {
     globals: true,
